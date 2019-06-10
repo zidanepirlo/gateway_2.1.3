29f3f5d44ff8aaac950385ecf49d175d41ceb052
yuan <zidanepirlo@163.com>
2019-06-10 15:16:14 +0800

    config for nacos

--- src/main/java/com/yuan/springcloud/scsrv/gateway/Application.java
+++ src/main/java/com/yuan/springcloud/scsrv/gateway/Application.java
@@ -4,15 +4,19 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableTransactionManagement
 @SpringBootApplication(exclude =
 		{
 				org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-				DataSourceAutoConfiguration.class
+				DataSourceAutoConfiguration.class,
+				EurekaClientAutoConfiguration.class
 		})
 @MapperScan(basePackages = "com.yuan.springcloud.scsrv.gateway.dao.domain.**",sqlSessionFactoryRef = "sqlSessionFactory")
 public class Application {
