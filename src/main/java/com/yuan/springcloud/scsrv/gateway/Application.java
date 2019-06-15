932662ec9c12a06a3b5bdcdf30b31b470356f72c
yuan <zidanepirlo@163.com>
2019-06-15 21:49:23 +0800

    delete zuul maven depend

--- src/main/java/com/yuan/springcloud/scsrv/gateway/Application.java
+++ src/main/java/com/yuan/springcloud/scsrv/gateway/Application.java
@@ -7,13 +7,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 //@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableTransactionManagement
-@EnableZuulProxy
 @SpringBootApplication(exclude =
 		{
 				org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
